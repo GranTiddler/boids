e50c65386873d2847f0a9bbbef0266061054e39b
skAdOOdlEs7 <grantdriddle7@gmailcom>
2022-02-03 12:07:38 -0700

    gets the chunk that each boid is in.

--- main.cpp
+++ main.cpp
@@ -13,7 +13,6 @@ class Boid : public sf::Sprite
 
         float floatMod(float num, float mod)
             {
-                std::cout<<num - floor(num / mod) * mod << std::endl;
                 return num - floor(num / mod) * mod;
             }
 
@@ -22,6 +21,14 @@ class Boid : public sf::Sprite
         float posY;
         float rotation;
         float speed;
+
+        Boid(){
+
+            
+            posX = 100;
+            posY = 100;
+            speed = 1;
+        }
 
         Boid(int x, int y, float angle, float s)
         {
@@ -103,22 +110,53 @@ class Boid : public sf::Sprite
 
 };
 
+float floatMod(float num, float mod)
+{
+    return num - floor(num / mod) * mod;
+}
 
+float dist(Boid p1, Boid p2){
+    int x1 = p1.posX;
+    int x2 = p2.posX;
 
-
+    int y1 = p1.posY;
+    int y2 = p2.posY;
+    return sqrt(pow((x1 - x2), 2) + pow(y1 - y2, 2));
+}
 
 int main() 
 {
+    int numChilds = 1000;
+    int chunks = 10;
+
+    int range = 10;
     srand(time(0));
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "minesweeper", sf::Style::Close);
 
-    Boid boy(800, 200, M_PI * .4, 3);
+    Boid children[numChilds];
+
     sf::Texture text;
     text.loadFromFile("boi.png");
-    boy.setTexture(text);
+    
+    for(int i = 0; i < numChilds; i++)
+    {
 
-    boy.setScale(0.5, 0.5);
+        
+        children[i] = Boid(200, 200, 0, 2);
+        children[i].setTexture(text);
+        children[i].setScale(.1, .1);
+        children[i].rotate(rand() * M_PI * 2 / RAND_MAX);
 
+        
+        
+        
+        
+    }
+    
+    int chunkList[numChilds];
+    int xChunk;
+    int yChunk;
+    
     
     window.setFramerateLimit(60);
     while (window.isOpen())
@@ -131,19 +169,27 @@ int main()
                 window.close();
         }
         
-        window.clear(sf::Color::White);
-
-        boy.move();
-        boy.rotateRand();
-        boy.avoidWalls();
-        window.draw(boy);
+        
+        window.clear(sf::Color(255, 255, 255));
         
 
+        for(int i = 0; i < numChilds; i++){
+            children[i].move();
+            children[i].rotateRand();
+            children[i].avoidWalls();
+            window.draw(children[i]);
 
+            xChunk = (children[i].posX - (int)floatMod(children[i].posX, 1000/chunks)) / chunks;
+            yChunk = (children[i].posY - (int)floatMod(children[i].posY, 1000/chunks)) / chunks;
 
+            chunkList[i] = (yChunk * chunks) + xChunk;
+        }
+        
+        
+        
+        
 
-
-
+        
 
 
         window.display();
