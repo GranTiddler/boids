b766cf71967adf4e10c7743263961b96417a8617
skAdOOdlEs7 <grantdriddle7@gmailcom>
2022-02-03 19:50:45 -0700

    alignment works now

--- main.cpp
+++ main.cpp
@@ -4,221 +4,223 @@
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include <ctime>
-#include <math.h> 
+#include <math.h>
 
 class Boid : public sf::Sprite
 {
-    private:
-        float max_turn = 0.03;
-        int wallAvoid = 100;
+private:
+	float max_turn = 0.03;
+	int wallAvoid = 100;
 
-        float floatMod(float num, float mod)
-            {
-                return num - floor(num / mod) * mod;
-            }
+	float floatMod(float num, float mod)
+	{
+		return num - floor(num / mod) * mod;
+	}
 
-    public:
-        float posX;
-        float posY;
-        float rotation;
-        float speed;
+public:
+	float posX;
+	float posY;
+	float rotation;
+	float speed;
 
-        Boid(){
+	Boid()
+	{
 
-            
-            posX = 100;
-            posY = 100;
-            speed = 1;
-        }
+		posX = 100;
+		posY = 100;
+		speed = 1;
+	}
 
-        Boid(int x, int y, float angle, float s)
-        {
-            posX = x;
-            posY = y;
-            rotate(angle);
-            speed = s;
-            
-        }
+	Boid(int x, int y, float angle, float s)
+	{
+		posX = x;
+		posY = y;
+		rotate(angle);
+		speed = s;
+	}
 
-        void move()
-        {
-            posX += cos(rotation) * speed;
-            posY += sin(rotation) * speed;
+	void move()
+	{
+		posX += cos(rotation) * speed;
+		posY += sin(rotation) * speed;
 
-            setPosition(posX, posY);
-        }
-        
-        void rotate(float angle)
-        {
-            sf::Sprite::rotate(angle * (360/ (M_PI * 2)));
-            rotation += angle;
-        }
+		setPosition(posX, posY);
+	}
 
-        void rotateRand(){
-            rotate(((float)rand() * max_turn * 2/RAND_MAX) - max_turn);
-            
-        }
+	void rotate(float angle)
+	{
+		sf::Sprite::rotate(angle * (360 / (M_PI * 2)));
+		rotation += angle;
+	}
 
-        void avoidWalls()
-        {
-            if(posX < 100)
-            {
-                if(floatMod(rotation, M_PI * 2) < M_PI)
-                {
-                    rotate(-.1 * wallAvoid / posX);
-                }
-                else
-                {
-                    rotate(.1 * wallAvoid / posX);
-                }
-            }
-            else if(posX > 900)
-            {
-                if(floatMod(rotation, M_PI * 2) > M_PI)
-                {
-                    rotate(-.1 * wallAvoid / (1000 - posX));
-                }
-                else
-                {
-                    rotate(.1 * wallAvoid / (1000 - posX));
-                }
-            }
+	void rotateRand()
+	{
+		rotate(((float)rand() * max_turn * 2 / RAND_MAX) - max_turn);
+	}
 
+	void avoidWalls()
+	{
+		if (posX < 100)
+		{
+			if (floatMod(rotation, M_PI * 2) < M_PI)
+			{
+				rotate(-.1 * wallAvoid / posX);
+			}
+			else
+			{
+				rotate(.1 * wallAvoid / posX);
+			}
+		}
+		else if (posX > 900)
+		{
+			if (floatMod(rotation, M_PI * 2) > M_PI)
+			{
+				rotate(-.1 * wallAvoid / (1000 - posX));
+			}
+			else
+			{
+				rotate(.1 * wallAvoid / (1000 - posX));
+			}
+		}
 
-            if(posY < 100)
-            {
-                if(floatMod(rotation +  M_PI_2, M_PI * 2) > M_PI)
-                {
-                    rotate(-.1 * wallAvoid / posY);
-                }
-                else
-                {
-                    rotate(.1 * wallAvoid / posY);
-                }
-            }
-            else if(posY > 900)
-            {
-                if(floatMod(rotation +  M_PI_2, M_PI * 2) < M_PI)
-                {
-                    rotate(-.1 * wallAvoid / (1000 - posY));
-                }
-                else
-                {
-                    rotate(.1 * wallAvoid / (1000 - posY));
-                }
-            }
-        }
-
+		if (posY < 100)
+		{
+			if (floatMod(rotation + M_PI_2, M_PI * 2) > M_PI)
+			{
+				rotate(-.1 * wallAvoid / posY);
+			}
+			else
+			{
+				rotate(.1 * wallAvoid / posY);
+			}
+		}
+		else if (posY > 900)
+		{
+			if (floatMod(rotation + M_PI_2, M_PI * 2) < M_PI)
+			{
+				rotate(-.1 * wallAvoid / (1000 - posY));
+			}
+			else
+			{
+				rotate(.1 * wallAvoid / (1000 - posY));
+			}
+		}
+	}
 };
 
 float floatMod(float num, float mod)
 {
-    return num - floor(num / mod) * mod;
+	return num - floor(num / mod) * mod;
 }
 
-float dist(Boid p1, Boid p2){
-    int x1 = p1.posX;
-    int x2 = p2.posX;
-
-    int y1 = p1.posY;
-    int y2 = p2.posY;
-    return sqrt(pow((x1 - x2), 2) + pow(y1 - y2, 2));
-}
-
-int main() 
+float dist(Boid p1, Boid p2)
 {
-    int rad = 40;
+	int x1 = p1.posX;
+	int x2 = p2.posX;
 
-    int numChilds = 500;
-    int chunks = 10;
+	int y1 = p1.posY;
+	int y2 = p2.posY;
+	return sqrt(pow((x1 - x2), 2) + pow(y1 - y2, 2));
+}
 
-    int range = 10;
-    srand(time(0));
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "BOID", sf::Style::Close);
+int main()
+{
+	int rad = 50;
+	float alignment = .050;
 
-    Boid children[numChilds];
+	int numChilds = 500;
+	int chunks = 1000 / rad;
 
-    sf::Texture text;
-    text.loadFromFile("boi.png");
-    
-    for(int i = 0; i < numChilds; i++)
-    {
+	int range = 10;
+	srand(time(0));
+	sf::RenderWindow window(sf::VideoMode(1000, 1000), "BOID", sf::Style::Close);
 
-        
-        children[i] = Boid(200, 200, 0, 2);
-        children[i].setTexture(text);
-        children[i].setScale(.2, .2);
-        children[i].rotate(rand() * M_PI * 2 / RAND_MAX);
+	Boid children[numChilds];
 
-        
-        
-        
-        
-    }
-    
-    std::vector<int> chunkList[chunks][chunks];
-    int xChunk;
-    int yChunk;
-    
-    int nearby;
-    
-    window.setFramerateLimit(60);
-    while (window.isOpen())
-    {      
-        //if close window button clicked close window
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        
-        
-        window.clear(sf::Color(255, 255, 255));
-        
+	sf::Texture text;
+	text.loadFromFile("boi.png");
 
-        for(int i = 0; i < numChilds; i++){
-            children[i].move();
-            children[i].rotateRand();
-            children[i].avoidWalls();
-            window.draw(children[i]);
+	for (int i = 0; i < numChilds; i++)
+	{
+		children[i] = Boid( 100 + (((float)rand() / RAND_MAX) * 800), 100 + (((float)rand() / RAND_MAX) * 800), 0, 2);
+		children[i].setTexture(text);
+		children[i].setScale(.25, .25);
+		children[i].rotate(rand() * M_PI * 2 / RAND_MAX);
+	}
 
-            xChunk = (children[i].posX - (int)floatMod(children[i].posX, 1000/chunks)) / (1000/chunks);
-            yChunk = (children[i].posY - (int)floatMod(children[i].posY, 1000/chunks)) / (1000/chunks);
+	std::vector<int> chunkList[chunks][chunks];
+	int xChunk;
+	int yChunk;
 
-            if (xChunk < 0){
-                std::cout<<xChunk<<std::endl;
-            }
+	int nearby;
+	float avgRotation;
 
-            chunkList[xChunk][yChunk].push_back(i);
+	window.setFramerateLimit(60);
+	while (window.isOpen())
+	{
+		//if close window button clicked close window
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
-        }
+		window.clear(sf::Color(255, 255, 255));
 
-        
-        for(int i = 0; i < numChilds; i++)
-        {
-            nearby = 0;
-            xChunk = (children[i].posX - (int)floatMod(children[i].posX, 1000/chunks)) / (1000/chunks);
-            yChunk = (children[i].posY - (int)floatMod(children[i].posY, 1000/chunks)) / (1000/chunks);
-            for(int j = 0; j < chunkList[xChunk][yChunk].size(); j++)
-            {
-                if(dist(children[i], children[chunkList[xChunk][yChunk][j]]) < rad)
-                {
-                    nearby ++;
-                }
-            }
-            std::cout<<nearby<<std::endl;
-        }
+		for (int i = 0; i < numChilds; i++)
+		{
+			children[i].move();
+			children[i].rotateRand();
+			children[i].avoidWalls();
+			window.draw(children[i]);
 
+			xChunk = (children[i].posX - (int)floatMod(children[i].posX, 1000 / chunks)) / (1000 / chunks);
+			yChunk = (children[i].posY - (int)floatMod(children[i].posY, 1000 / chunks)) / (1000 / chunks);
 
-        for(int i = 0; i < chunks; i++)
-        {
-            for(int j = 0; j < chunks; j++)
-            {
-                chunkList[i][j].clear();
-            }
-        }
-        window.display();
-        
-    }
+			if (xChunk < 0)
+			{
+				std::cout << xChunk << std::endl;
+			}
+
+			chunkList[xChunk][yChunk].push_back(i);
+		}
+
+		for (int i = 0; i < numChilds; i++)
+		{
+			nearby = 0;
+			avgRotation = 0;
+			xChunk = (children[i].posX - (int)floatMod(children[i].posX, 1000 / chunks)) / (1000 / chunks);
+			yChunk = (children[i].posY - (int)floatMod(children[i].posY, 1000 / chunks)) / (1000 / chunks);
+			for (int x = 0; x <= 9; x++)
+			{
+
+				int xx = (x % 3) - 1;
+				int y = ((x - xx) / 3) - 1;
+				if (!(xChunk + xx < 0 || xChunk + xx + 1 > chunks || yChunk + y < 0 || yChunk + y + 1 > chunks))
+				{
+					for (int j = 0; j < chunkList[xChunk + xx][yChunk + y].size(); j++)
+					{
+
+						if (dist(children[i], children[chunkList[xChunk + xx][yChunk + y][j]]) < rad)
+						{
+							nearby++;
+							avgRotation += children[chunkList[xChunk + xx][yChunk + y][j]].rotation;
+						}
+					}
+				}
+			}
+			avgRotation /= nearby;
+			children[i].rotate((avgRotation - children[i].rotation) * alignment);
+		}
+
+		for (int i = 0; i < chunks; i++)
+		{
+			for (int j = 0; j < chunks; j++)
+			{
+				chunkList[i][j].clear();
+			}
+		}
+		window.display();
+	}
 }
